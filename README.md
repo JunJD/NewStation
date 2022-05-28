7498af1557d6599af6c3fec7b885126ce524c8cc
JunJD <864546065@qq.com>
2022-05-28 14:35:01 +0800

    newstation

---
+++ README.md
@@ -0,0 +1,28 @@
+样式解决方案？
+inline CSS ? 
+CSS in JS
+style Component
+sass/less
+
+样式文件格式？
+styles/
+    _variables.less=>各种变量和可配置设置
+    _mixins.less=>全局minis
+    _functions.less=>全局 functions
+components/
+    Button/
+        style.less=>组件单独的颜色
+
+样式色彩体系？
+系统色板--基础色板+中性色板
+产品色板--品牌色+功能色
+
+组件库样式变量分类？
+基础色彩系统
+字体系统
+表单
+按钮
+边框和阴影
+可配置开关
+
+
---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='heading'>
+        <h1>
+          111
+        </h1>
+      </div>
+      <div>
+        <h1>
+          111
+        </h1>
+      </div>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
